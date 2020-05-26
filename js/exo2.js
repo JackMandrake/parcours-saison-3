639f5498ca7c8d911300b5cd7d8fa1b6eee24c98
JackMandrake <coignard.cyrille@hotmail.fr>
2020-05-26 11:22:15 +0200

    exo2 complete

--- js/exo2.js
+++ js/exo2.js
@@ -5,63 +5,37 @@
  * 3. On affiche le résultat dans #result.
  */
 
-var input1 = document.getElementById('input1').value;
-var input2 = document.getElementById('input2').value;
-var stockResult = document.getElementById('result');
-var result;
+var appCalculator = {
+  // Propriétés du module en querySelector, j'aurais pu utiliser getElementById
+  input1: document.querySelector('#input1'),
+  input2: document.querySelector('#input2'),
+  inputFormElt: document.querySelector('#container'),
+  stockResult: document.querySelector('#result'),
 
-/* if ((input1 = !null && !isNaN(input1)) && (input2 = !null && !isNaN(input2))) {
-  result = input1 + input2;
-  stockResult.innerHTML = result;
-}
- */
+  init: function() {
+    appCalculator.inputFormElt.addEventListener('submit', appCalculator.handleCalculate);
+  }, 
 
-console.log(input1);
-console.log(input2);
-result = input1 + input2;
-stockResult.innerHTML = result;
-console.log(result);
+  handleCalculate: function(evt) {
+    // Recup des valeurs et on s'assure que ca soit des nombres en decimal
+    var number1 = appCalculator.input1.value;
+    var number2 = appCalculator.input2.value;
+    number1 = Number(number1, 10);
+    number2 = Number(number2, 10);
 
-
-
-/* var app = { */
-  // Propriétés du module
-  /* input1: document.getElementById('input1'),
-  input2: document.getElementById('input2'),
-  stockResult: document.getElementById('result'),
-  submit: document.getElementById('container'), */
-
-  // Fonction de récupération des valeurs
-  /* init: function() {
-    app.input1.addEventListener('focus',app.handleVerifInput1);
-    app.input2.addEventListener('focus',app.handleVerifInput2);
-    app.submit.addEventListener('submit',app.handleCalculate);
-  }, */
-  // Fonction de vérification de la 1ere valeure
-  /* handleVerifInput1: function() {
-    let isValid1 = app.input1 = !null && !isNaN(app.input1); 
-    
-    if (isValid1) {
-      return;
+    if (isNaN(number1) || isNaN(number2)) {
+      // Si les valeurs entrées ne sont pas des nombres, voir à rajouter une condition pour qu'une
+      // valeure soit obligatoire dans chaque champ, avec !null par exemple
+      evt.preventDefault();
+      alert('Veuillez entrer des nombres');
+    } else{
+      // Si les valeurs entrées sont bien des nombres, on fait le calcul et on l'affiche dans la div result
+      var Result = number1 + number2;
+      appCalculator.stockResult.innerHTML = Result;
+      
+      // pour garder les valeurs dans mes champs :
+      evt.preventDefault();
     }
-  }, */
-  // Fonction de vérification de la 2ème valeure
-  /* handleVerifInput2: function() {
-    let isValid2 = app.input2 = !null && !isNaN(app.input2);
-
-    if (isValid2) {
-        return;
-    }
-  }, */
-  // Fonction de calcul et affichage
-  /* handleCalculate: function() {
-    result = app.input1.value + app.input2.value;
-    app.stockResult.innerHTML = result;
   }
-
 };
-
-app.init();
- */
-
-
+appCalculator.init();
